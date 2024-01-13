8a7f76bf2bde232c32493f984fa4e4b0386c28a8
zeekielcodes <ezekielabolade@gmail.com>
2024-01-13 05:48:59 +0100

    Nav Logo responsiveness

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Nav() {
   return (
     <nav className='px-4 md:px-8 lg:px-12 xl:px-40 py-2 flex justify-between items-center'>
-        <Link href="/"><Image src="/logo.png" height={50} width={70} className='h-[50px] w-auto ' alt='FoodFusion'/></Link>
+        <Link href="/"><Image src="/logo.png" height={50} width={70} className='h-[20px] md:h-[30px] lg:h-[50px] w-auto ' alt='FoodFusion'/></Link>
        
         <Link href="/docs">Docs</Link>
      
